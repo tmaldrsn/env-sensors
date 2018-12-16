09fa13fe391fe5a34b444cb3f65d2e59565d1725
Tim Alderson <tmaldrsn@gmail.com>
2018-12-15 21:16:42 -0500

    trying to write to a file

--- test_run.py
+++ test_run.py
@@ -1,6 +1,9 @@
+import os
 import serial
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
+
 	arduino = serial.Serial('/dev/ttyACM0', timeout=1, baudrate=9600)
-	while True:
-		print(str(arduino.readline()))
+	with open('data.txt', 'w+') as f:
+		while True:
+			f.write(str(arduino.readline()))
