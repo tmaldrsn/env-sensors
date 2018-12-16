26c7d3580e9cddf8f88c6131b9878d9feb7ad017
Tim Alderson <tmaldrsn@gmail.com>
2018-12-15 22:03:29 -0500

    updated formatting script

--- format_text.py
+++ format_text.py
@@ -4,17 +4,30 @@ if __name__ == "__main__":
 	with open('data.txt', 'r+') as f:
 		text = f.read()
 
-	result_array = text.split("\\r\\n'b'")[3:]
-	
+	result_array = text.split("\\r\\n'b'")[3:-2]
 
-	temps = []
-	humds = []
+	temp_lines = []
+	humd_lines = []
 
 	for result in result_array:
 		if result[0] == "T":
-			temps.append(result)
+			temp_lines.append(result)
 		elif result[0] == "H":
-			humds.append(result)
+			humd_lines.append(result)
 
-	print('\n'.join(temps))
-	print('\n'.join(humds))
+	if humd_lines[-1] == "'":
+		humd_lines[-1] = humd_lines[-1][:-7]
+	if temp_lines[-1] == "'":
+		temp_lines[-1] == temp_lines[-1][:-7]
+
+	#print('\n'.join(temp_lines))
+	#print('\n'.join(humd_lines))
+
+	temps = []
+	humds = []	
+	for temp in temp_lines:
+		temps.append(float(temp.split()[-1]))
+	for humd in humd_lines:
+		humds.append(float(humd.split()[-1]))
+
+	print(temps, humds)
