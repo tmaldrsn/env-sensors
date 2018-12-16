2b35d1a8fc6ab7531a1917f211925a6129e7742e
Tim Alderson <tmaldrsn@gmail.com>
2018-12-15 21:29:08 -0500

    output should be multiple lines now..?

--- test_run.py
+++ test_run.py
@@ -4,6 +4,6 @@ import serial
 if __name__ == "__main__": 
 
 	arduino = serial.Serial('/dev/ttyACM0', timeout=1, baudrate=9600)
-	with open('data.txt', 'w+') as f:
-		while True:
+	while True:
+		with open('data.txt', 'w+') as f:
 			f.write(str(arduino.readline()))
