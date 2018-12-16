b46d82bd65c555c30be8c4cd98362518fcac6ad6
Tim Alderson <tmaldrsn@gmail.com>
2018-12-15 23:31:56 -0500

    adding timestamps from python end

--- test_run.py
+++ test_run.py
@@ -1,12 +1,18 @@
 import os
+import time
 import serial
 
+
 if __name__ == "__main__": 
+
+	start_time = time.time()
 
 	arduino = serial.Serial('/dev/ttyACM0', timeout=1, baudrate=9600)
 	with open('data.txt', 'w+') as f:
 		while True:	
 			if arduino.readline().decode() != "":
+				data_time = time.time() - start_time()	
 				f.write('\n')
-				print(str(arduino.readline().decode()))	
-				f.write(str(arduino.readline().decode()))
+				
+				print(data_time, str(arduino.readline().decode()))	
+				f.write(str(data_time) + " " + str(arduino.readline().decode()))
