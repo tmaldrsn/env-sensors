a421859ca74b9d022a0653ad191e90b004fb7baa
Tim Alderson <tmaldrsn@gmail.com>
2018-12-15 23:12:52 -0500

    trying to get decoded data

--- test_run.py
+++ test_run.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
 	arduino = serial.Serial('/dev/ttyACM0', timeout=1, baudrate=9600)
 	with open('data.txt', 'w+') as f:
 		while True:	
-			print(str(arduino.readline()))	
-			f.write(str(arduino.readline()))
+			if arduino.readline().decode() != "":
+				f.write('\n')
+				print(str(arduino.readline().decode()))	
+				f.write(str(arduino.readline().decode()))
